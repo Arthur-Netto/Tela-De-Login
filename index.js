484af1200b8980914a73df33b2b9b314ca4dda78
Arthur-Netto <arthurbarroconeto13@gmail.com>
2024-04-26 23:06:27 -0400

    primeira publicação

---
+++ index.js
@@ -0,0 +1,9 @@
+function capturarInformacoes(){
+    let usuario = document.getElementById('Usuario').value;
+    let senha = document.getElementById('Senha').value;
+
+    alert ("Usuário: " + usuario );
+}
+
+
+
